1213fa9ca1214537a7d5293f64da4b7feb1a7179
Julian Fernandez <fernandezjulian00@hotmail.com>
2024-08-14 22:28:57 -0300

    Agregar archivos de proyecto.

---
+++ FormularioWebPrg3/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FormularioWebPrg3
+{
+	public partial class _Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+			if (!IsPostBack)
+			{
+				lblMensaje.Text = "Hola Mundo - Es la primera vez que se ha cargado la página";
+
+				Session["Recargas"] = 0;
+			}
+			else
+			{
+				lblMensaje.Text = "Esta página ya ha sido visitada";
+
+				Session["Recargas"] = (int)Session["Recargas"] + 1;
+			}
+
+			lblContadorUsuarios.Text = "Cantidad de usuarios que accedieron a la app: " + Application["UserCount"].ToString();
+
+			lblContadorRecargas.Text = "Veces que has recargado la página: " + Session["Recargas"].ToString();
+		}
+
+		protected void btnRecargar_Click(object sender, EventArgs e)
+		{
+
+			Response.Redirect(Request.RawUrl);
+
+		}
+	}
+}
---
+++ FormularioWebPrg3/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace FormularioWebPrg3
+{
+	public class Global : HttpApplication
+	{
+		void Application_Start(object sender, EventArgs e)
+		{
+			// Código que se ejecuta al iniciar la aplicación
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			Application["UserCount"] = 0;
+		}
+
+		protected void Session_Start(object sender, EventArgs e)
+		{
+			Application.Lock();
+			Application["UserCount"] = (int)Application["UserCount"] + 1;
+			Application.UnLock();
+		}
+	}
+}
